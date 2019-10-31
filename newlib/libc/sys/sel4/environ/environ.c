0d8db77042f32228c9332adcc91b002379fe60be
alguacilaguamara <alejandro.alguacil@wazuh.com>
2019-10-31 12:02:28 +0100

    Fixed var environ

--- newlib/libc/sys/sel4/environ/environ.c
+++ newlib/libc/sys/sel4/environ/environ.c
@@ -1,5 +1,12 @@
- //por aki creo q: https://github.com/seL4/seL4_libs/blob/ce2bfdd3fb2ad949f21ea5e24efa3fa1a5fe801c/libsel4muslcsys/src/vsyscall.c
+//https://github.com/seL4/seL4_libs/blob/ce2bfdd3fb2ad949f21ea5e24efa3fa1a5fe801c/libsel4muslcsys/src/vsyscall.c
 //sel4_vsyscall
 //https://github.com/seL4/musllibc/blob/b41b6f8ff99a4328a681023b64234938459854fc/src/env/__environ.c
 //https://github.com/seL4/musllibc/blob/b41b6f8ff99a4328a681023b64234938459854fc/src/internal/libc.h
-char **environ; /* pointer to array of char * strings that define the current environment variables */
+// char **environ; /* pointer to array of char * strings that define the current environment variables */
+
+#include "libc.h"
+
+char **environ = 0;
+weak_alias(environ, ___environ);
+weak_alias(environ, _environ);
+weak_alias(environ, environ);
